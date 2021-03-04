5638173e74cb2c9c110928001346ac7717c56424
arbiterdata <54211294+arbiterdata@users.noreply.github.com>
2021-03-04 22:20:49 +0000

    staticfiles directory

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,8 +125,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 
 STATICFILES_DIRS = (
-    [BASE_DIR,'assets']
+    #[BASE_DIR,'assets']
+    os.path.join(BASE_DIR,'assets'),
+
 )
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
