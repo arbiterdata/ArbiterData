0fa5f9799b7f65342e9bee96b6bb2e6a6166d507
arbiterdata <54211294+arbiterdata@users.noreply.github.com>
2021-03-07 14:59:14 +0000

    commit https

--- mysite/settings.py
+++ mysite/settings.py
@@ -64,7 +64,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SECURE_SSL_REDIRECT = True
 PREPEND_WWW = True
 
